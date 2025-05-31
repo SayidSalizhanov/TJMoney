c3f40529b279790c5d86311cbcb5b4a18d9baac0
Sayid <sayid.salizhanov@mail.ru>
2025-05-31 19:32:35 +0300

    add modified objectmapper bean

--- rest-api/impl/src/main/java/ru/itis/impl/config/AppConfig.java
+++ rest-api/impl/src/main/java/ru/itis/impl/config/AppConfig.java
@@ -1,6 +1,10 @@
 package ru.itis.impl.config;
 
 import com.cloudinary.Cloudinary;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -8,6 +12,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import ru.itis.impl.properties.CloudinaryProperties;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -16,6 +22,8 @@ import java.util.Map;
 @EnableConfigurationProperties(CloudinaryProperties.class)
 @RequiredArgsConstructor
 public class AppConfig {
+
+    private static final String DATE_TIME_PATTERN = "yyyy-MM-dd HH:mm:ss";
 
     private final CloudinaryProperties cloudinaryProperties;
 
@@ -26,5 +34,21 @@ public class AppConfig {
         config.put("api_key", cloudinaryProperties.getApiKey());
         config.put("api_secret", cloudinaryProperties.getApiSecret());
         return new Cloudinary(config);
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        JavaTimeModule javaTimeModule = new JavaTimeModule();
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_TIME_PATTERN);
+
+        javaTimeModule.addSerializer(LocalDateTime.class,
+                new LocalDateTimeSerializer(formatter));
+        javaTimeModule.addDeserializer(LocalDateTime.class,
+                new LocalDateTimeDeserializer(formatter));
+
+        objectMapper.registerModule(javaTimeModule);
+        return objectMapper;
     }
 }
