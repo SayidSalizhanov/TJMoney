4de802b4470028e32aea631d3540183491ea89d7
Sayid <sayid.salizhanov@mail.ru>
2025-06-02 14:54:53 +0300

    change resttemplate using in goalservice

--- main-service/src/main/java/ru/itis/mainservice/service/GoalService.java
+++ main-service/src/main/java/ru/itis/mainservice/service/GoalService.java
@@ -2,8 +2,7 @@ package ru.itis.mainservice.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -12,6 +11,7 @@ import ru.itis.mainservice.dto.request.goal.GoalSettingsRequest;
 import ru.itis.mainservice.dto.response.goal.GoalListResponse;
 import ru.itis.mainservice.dto.response.goal.GoalSettingsResponse;
 
+import java.util.Collections;
 import java.util.List;
 
 @Service
@@ -19,6 +19,7 @@ import java.util.List;
 public class GoalService {
 
     private final RestTemplate restTemplate;
+    private final AuthService authService;
     private final String BASE_URL = "http://localhost:8080/api/goals";
 
     public GoalSettingsResponse getGoal(Long id, Long userId) {
@@ -27,7 +28,19 @@ public class GoalService {
                 .buildAndExpand(id)
                 .toUriString();
 
-        return restTemplate.getForObject(url, GoalSettingsResponse.class);
+        HttpHeaders headers = authService.getAuthHeaders();
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+
+        HttpEntity<Void> entity = new HttpEntity<>(headers);
+
+        ResponseEntity<GoalSettingsResponse> response = restTemplate.exchange(
+                url,
+                HttpMethod.GET,
+                entity,
+                GoalSettingsResponse.class
+        );
+
+        return response.getBody();
     }
 
     public void updateGoalInfo(Long id, Long userId, GoalSettingsRequest request) {
@@ -36,7 +49,17 @@ public class GoalService {
                 .buildAndExpand(id)
                 .toUriString();
 
-        restTemplate.put(url, request);
+        HttpHeaders headers = authService.getAuthHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<GoalSettingsRequest> entity = new HttpEntity<>(request, headers);
+
+        restTemplate.exchange(
+                url,
+                HttpMethod.PUT,
+                entity,
+                Void.class
+        );
     }
 
     public void deleteGoal(Long id, Long userId) {
@@ -45,7 +68,11 @@ public class GoalService {
                 .buildAndExpand(id)
                 .toUriString();
 
-        restTemplate.delete(url);
+        HttpHeaders headers = authService.getAuthHeaders();
+
+        HttpEntity<Void> entity = new HttpEntity<>(headers);
+
+        restTemplate.exchange(url, HttpMethod.DELETE, entity, Void.class);
     }
 
     public List<GoalListResponse> getGoals(
@@ -63,10 +90,15 @@ public class GoalService {
             builder.queryParam("groupId", groupId);
         }
 
+        HttpHeaders headers = authService.getAuthHeaders();
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+
+        HttpEntity<Void> entity = new HttpEntity<>(headers);
+
         ResponseEntity<List<GoalListResponse>> response = restTemplate.exchange(
                 builder.toUriString(),
                 HttpMethod.GET,
-                null,
+                entity,
                 new ParameterizedTypeReference<>() {}
         );
 
@@ -81,10 +113,16 @@ public class GoalService {
             builder.queryParam("groupId", groupId);
         }
 
-        return restTemplate.postForObject(
+        HttpHeaders headers = authService.getAuthHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<GoalCreateRequest> entity = new HttpEntity<>(request, headers);
+
+        return restTemplate.exchange(
                 builder.toUriString(),
-                request,
+                HttpMethod.POST,
+                entity,
                 Long.class
-        );
+        ).getBody();
     }
 }
