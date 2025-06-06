1d7e97315dd0d146e8d085a79b14c318c234d048
Sayid <sayid.salizhanov@mail.ru>
2025-06-06 17:49:32 +0300

    remove okhttp library, using resttemplate

--- rest-api/api/src/main/java/ru/itis/dto/response/fxrates/FxRatesResponse.java
+++ rest-api/api/src/main/java/ru/itis/dto/response/fxrates/FxRatesResponse.java
@@ -1,10 +1,12 @@
 package ru.itis.dto.response.fxrates;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Builder;
 
 import java.util.Map;
 
 @Builder
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record FxRatesResponse (
         Boolean success,
         String timestamp,
