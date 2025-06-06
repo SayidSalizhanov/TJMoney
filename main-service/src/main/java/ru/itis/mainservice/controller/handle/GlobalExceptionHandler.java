d765365d748bfa188ad9d943698fe977282f3d8d
Sayid <sayid.salizhanov@mail.ru>
2025-06-06 20:47:15 +0300

    add error handling more effective

--- main-service/src/main/java/ru/itis/mainservice/controller/handle/GlobalExceptionHandler.java
+++ main-service/src/main/java/ru/itis/mainservice/controller/handle/GlobalExceptionHandler.java
@@ -12,7 +12,8 @@ import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.method.annotation.HandlerMethodValidationException;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.multipart.MaxUploadSizeExceededException;
-import org.springframework.web.servlet.view.RedirectView;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 import ru.itis.mainservice.dto.response.exception.ExceptionMessage;
 
 import java.util.ArrayList;
@@ -25,89 +26,92 @@ public class GlobalExceptionHandler {
 
     private static final String FILE_SIZE_EXCEEDED = "Размер файла превышает допустимый предел в 5MB";
 
+    @ExceptionHandler(NoResourceFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ModelAndView handleNoResourceFoundException(NoResourceFoundException exception) {
+        ModelAndView modelAndView = new ModelAndView("error");
+        modelAndView.addObject("message", "Страница не найдена");
+        return modelAndView;
+    }
+
     @ExceptionHandler(HttpClientErrorException.class)
-    public RedirectView handleHttpClientErrorException(HttpClientErrorException exception) {
+    public ModelAndView handleHttpClientErrorException(HttpClientErrorException exception) {
         if (exception.getStatusCode() == HttpStatus.UNAUTHORIZED || 
             exception.getStatusCode() == HttpStatus.FORBIDDEN) {
-            return new RedirectView("/login");
+            return new ModelAndView("redirect:/login");
         }
-        return null;
+        if (exception.getStatusCode() == HttpStatus.NOT_FOUND) {
+            ModelAndView modelAndView = new ModelAndView("error");
+            modelAndView.addObject("message", "Страница не найдена");
+            return modelAndView;
+        }
+        ModelAndView modelAndView = new ModelAndView("error");
+        modelAndView.addObject("message", "Произошла ошибка при обработке запроса");
+        return modelAndView;
     }
 
     // обработчик для исключений, когда не можем преобразовать значение аргумента метода контроллера в ожидаемый тип
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public final ExceptionMessage handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException exception) {
-        return ExceptionMessage
-                .builder()
-                .exceptionName(exception.getClass().getSimpleName())
-                .message(exception.getMessage())
-                .build();
+    public ModelAndView handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException exception) {
+        ModelAndView modelAndView = new ModelAndView("error");
+        modelAndView.addObject("message", exception.getMessage());
+        return modelAndView;
     }
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public final ExceptionMessage handleHttpMessageNotReadableException(HttpMessageNotReadableException exception) {
-        return ExceptionMessage
-                .builder()
-                .exceptionName(exception.getClass().getSimpleName())
-                .message(exception.getMessage())
-                .build();
+    public ModelAndView handleHttpMessageNotReadableException(HttpMessageNotReadableException exception) {
+        ModelAndView modelAndView = new ModelAndView("error");
+        modelAndView.addObject("message", exception.getMessage());
+        return modelAndView;
     }
 
     // обработчик для исключений, когда картинка слишком большая
     @ExceptionHandler(MaxUploadSizeExceededException.class)
     @ResponseStatus(HttpStatus.PAYLOAD_TOO_LARGE)
-    public final ExceptionMessage handleMaxUploadSizeExceededException(MaxUploadSizeExceededException exception) {
-        return ExceptionMessage
-                .builder()
-                .exceptionName(exception.getClass().getSimpleName())
-                .message(FILE_SIZE_EXCEEDED)
-                .build();
+    public ModelAndView handleMaxUploadSizeExceededException(MaxUploadSizeExceededException exception) {
+        ModelAndView modelAndView = new ModelAndView("error");
+        modelAndView.addObject("message", FILE_SIZE_EXCEEDED);
+        return modelAndView;
     }
 
     // обработчик для исключений, когда происходит ошибка валидации методов или параметров контроллера
     @ExceptionHandler(HandlerMethodValidationException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public final ExceptionMessage handleMethodValidationException(HandlerMethodValidationException exception) {
+    public ModelAndView handleMethodValidationException(HandlerMethodValidationException exception) {
         Map<String, List<String>> errors = new HashMap<>();
         List<ParameterValidationResult> ex = exception.getParameterValidationResults();
         for (ParameterValidationResult result : ex) {
             String parameterName = result.getMethodParameter().getParameterName();
             errors.put(parameterName, result.getResolvableErrors().stream().map(MessageSourceResolvable::getDefaultMessage).toList());
-
         }
-        return ExceptionMessage.builder()
-                .message(exception.getMessage())
-                .errors(errors)
-                .exceptionName(exception.getClass().getSimpleName())
-                .build();
+        ModelAndView modelAndView = new ModelAndView("error");
+        modelAndView.addObject("message", "Ошибка валидации: " + errors);
+        return modelAndView;
     }
 
     // обработчик для исключений, когда не проходит валидация объекта DTO
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public final ExceptionMessage handleValidationException(MethodArgumentNotValidException exception) {
+    public ModelAndView handleValidationException(MethodArgumentNotValidException exception) {
         Map<String, List<String>> errors = new HashMap<>();
         exception.getBindingResult().getFieldErrors().forEach(error -> {
             errors.computeIfAbsent(error.getField(), k -> new ArrayList<>())
                     .add(error.getDefaultMessage());
         });
-        return ExceptionMessage.builder()
-                .message(exception.getMessage())
-                .errors(errors)
-                .exceptionName(exception.getClass().getSimpleName())
-                .build();
+        ModelAndView modelAndView = new ModelAndView("error");
+        modelAndView.addObject("message", "Ошибка валидации: " + errors);
+        return modelAndView;
     }
 
     // обработчик для всех остальных исключений, которых не должно было быть
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public final ExceptionMessage onAllExceptions(Exception exception) {
+    public ModelAndView onAllExceptions(Exception exception) {
         exception.printStackTrace();
-        return ExceptionMessage.builder()
-                .message(exception.getMessage())
-                .exceptionName(exception.getClass().getSimpleName())
-                .build();
+        ModelAndView modelAndView = new ModelAndView("error");
+        modelAndView.addObject("message", "Внутренняя ошибка сервера");
+        return modelAndView;
     }
 }
